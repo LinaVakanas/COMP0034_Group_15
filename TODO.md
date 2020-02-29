dcfb4ca9d2840f83c3f133caa557b6bab94eb9d8
LinaVakanas <56156107+LinaVakanas@users.noreply.github.com>
2020-02-29 17:43:17 +0000

    updates forms.py, made to do list

---
+++ TODO.md
@@ -0,0 +1,10 @@
+### Things to do ###
+* What goes in 
+    - medication form
+    - personal issues
+    - hobbies
+
+* How to make check box thing in python (forms.py)
+* Decide which ones we're going to make in forms.py and which ones will just be on html using bootstrap => if have time learn how to use forms w bootstrap in python
+* Register link sent => do we make route for it and then put /<rand_num>/ at the end or something?
+
--- app/main/forms.py
+++ app/main/forms.py
@@ -10,6 +10,24 @@ class MentorSignupForm(FlaskForm):
     ## file for DBS ##
 
 
+class PersonalForm(Form):
+    carer_name = StringField("Carer's full name:", validators=DataRequired())
+    carer_email = StringField("Carer's email address:", validators=[DataRequired(), Email()])
+    choices1 = [('Football', 'Football'),
+                ('Drawing', 'Drawing')]  # whatever we decide to use
+    hobbies = SelectField('', choices=choices1)
+    # personality #
+    choices2 = [('???', '???')]  # search what medical stuff usually have
+    medical_cond = SelectField('', choices=choices2)
+    choices3 = [('Depression', 'Depression'),
+                ('Self-harm', 'Self-harm'),
+                ('Family problems', 'Family problems'),
+                ('Drugs', 'Drugs'),
+                ('Eating disorder', 'Eating disorder')]  # this was what i could remember off the top of my head
+    personal_issues = SelectField('', choices=choices3)
+    choices4 = ['University student']
+    mentor_occupation =
+
 
 
 class CityBlogSearchForm(Form):
