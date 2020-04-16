ee8b5b00d3e2228fa5315e9de70df84768060e80
LinaVakanas <56156107+LinaVakanas@users.noreply.github.com>
2020-04-16 12:20:52 +0100

    updating

--- app/test/test_basic.py
+++ app/test/test_basic.py
@@ -42,12 +42,7 @@ class BaseTest(TestCase):
         self.mentee = Mentee(user_id=2, school_id=1, first_name='Lily', last_name='Weasley', paired=True)
         self.mentee_location = Location(user_id=2, address="Hogmeade", city="London",
                                         postcode="XR4 5AQ", avoid_area="Neasden")
-        # PAIR
-        self.pair = Pair(mentor_id=self.mentor.mentor_id, mentee_id=self.mentee.mentee_id)
-        # MEETING
-        self.meeting = Meeting(pair_id=1, date='5/5/2020', time='1700',
-                               duration='1', address="Kilburn Road", postcode="XY4 5UU", type="Library",
-                               mentee_approval=False)
+
 
         # --------TO BE PAIRED MENTEE/MENTOR --------
         # MENTOR - Active, approved, location form only, unpaired
@@ -97,6 +92,13 @@ class BaseTest(TestCase):
         db.session.add_all([self.user7, self.mentor4, self.mentor4_personal_info])
         db.session.add_all([self.user0, self.admin])
         db.session.flush()
+
+        # PAIR
+        self.pair = Pair(mentor_id=self.mentor.mentor_id, mentee_id=self.mentee.mentee_id)
+        # MEETING
+        self.meeting = Meeting(pair_id=1, date='5/5/2020', time='1700',
+                               duration='1', address="Kilburn Road", postcode="XY4 5UU", type="Library",
+                               mentee_approval=False)
         db.session.add_all([self.pair, self.meeting])
         db.session.commit()
 
@@ -126,25 +128,25 @@ class BaseTest(TestCase):
         )
 
     # test mentee and mentor details:
-    mentee_data = dict(user_id=8, user_type='mentee', first_name='Hermione', last_name='Granger', school_id=1,
+    mentee_data = dict(user_id=4, user_type='mentee', first_name='Hermione', last_name='Granger', school_id=1,
                        password='password3', email='test@mail.com')
-    mentee_personal_issues_data = dict(user_id=8, depression=False, self_harm=True, family=True, drugs=False, ed=True,)
+    mentee_personal_issues_data = dict(user_id=4, depression=False, self_harm=True, family=True, drugs=False, ed=True,
+                                       )
     mentee_personal_info = dict(carer_email='emma@gmail.com', carer_name='Emma Granger', share_performance=False,
-                                user_id=8, share_personal_issues=True)
-    mentee_hobbies = dict(user_id=8, football=True, drawing=False)
+                                user_id=4, share_personal_issues=True)
+    mentee_hobbies = dict(user_id=4, football=True, drawing=False)
 
-    mentor_data = dict(user_id=9, user_type='mentor', first_name='Ron', last_name='Weasley', school_id=0,
+    mentor_data = dict(user_id=5, user_type='mentor', first_name='Ron', last_name='Weasley', school_id=0,
                        password='password4', email='test2@mail.com')
-    mentor_personal_issues_data = dict(user_id=9, depression=True, self_harm=True, drugs=True, ed=True)
-    mentor_personal_info = dict(carer_email='', carer_name='', status='W', xperience='>=2', user_id=5, share_personal_issues=True)
-    mentor_hobbies = dict(user_id=9, football=True)
-    mentor4_location_data = dict(user_id=9, address="Oxford Street", city="London",
-                                 postcode="XR4 5AQ", avoid_area="Kilburn")
 
+    mentor_personal_issues_data = dict(user_id=5, depression=True, self_harm=True, drugs=True, ed=True)
+    mentor_personal_info = dict(carer_email='', carer_name='', status='W', xperience='>=2', user_id=5, share_personal_issues=True)
+    mentor_hobbies = dict(user_id=5, football=True)
     book_meeting = dict(pair_id=1, day='3', month='5', year=2020, date='3/5/2020', hour='17', minute='00', time='1700',
                         duration='1', address="Kilburn Road", postcode="WY4 5UU", type="Library")
 
-
+    mentor4_location_data = dict(user_id=7, address="Oxford Street", city="London",
+                                     postcode="XR4 5AQ", avoid_area="Kilburn")
 
 
 class TestMain(BaseTest):
